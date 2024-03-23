52e2673d987e4efb81333fe564fc6c78c9d3f26c
Rachael Momber <rlmomber@gmail.com>
2024-03-23 11:39:16 +0900

    add create method

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -1,9 +1,26 @@
 class Api::V1::CafesController < ApplicationController
 
   # '/api/v1/cafes'
+  # '/api/v1/cafes?title=name
   def index
-    @cafes = Cafe.all
+    if params[:title].present?
+      # filter
+      @cafes = Cafe.where('title ILIKE ?', "%#{params[:title]}%")
+    else
+      @cafes = Cafe.all
+    end
     # don't want to render HTML (i.e. render 'index.html.erb')
-    render json: @cafes
+    # order based on newest first
+    render json: @cafes.order(created_at: :desc)
+  end
+
+  def create
+    @cafe = Cafe.new(cafe_params)
+  end
+
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:title, :address, :picture, hours: {}, criteria: [])
   end
 end
